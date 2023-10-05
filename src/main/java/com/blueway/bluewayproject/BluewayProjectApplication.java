6adb2f362e7f1909d2343f87b0c8647a18df5a24
Wendel-Fl <wendelfl.dev@gmail.com>
2023-10-05 00:18:43 -0300

    New Project

---
+++ src/main/java/com/blueway/bluewayproject/BluewayProjectApplication.java
@@ -0,0 +1,13 @@
+package com.blueway.bluewayproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluewayProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluewayProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blueway/bluewayproject/BluewayProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blueway.bluewayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluewayProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
